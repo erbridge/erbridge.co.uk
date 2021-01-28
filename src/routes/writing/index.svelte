3d5bc7f61c37bbdfb47301ff3569397db2d7bcc2
erbridge <f@erbridge.co.uk>
2021-01-28 21:16:33 +0200

    Add extracts and summaries to the writing index

--- src/routes/writing/index.svelte
+++ src/routes/writing/index.svelte
@@ -2,38 +2,58 @@
   import { Head } from "@erbridge/website-theme";
 
   const pages = [
-    { title: "USER ERROR", slug: "writing/user-error" },
+    {
+      title: "USER ERROR",
+      slug: "writing/user-error",
+      extract:
+        "I looked at the pair of figures lying motionless in the dark, damp room. Water dripped from a pipe nearby and the smell of blood filled my nostrils",
+    },
     {
       title: "The Groaning Worm (Part II)",
       slug: "writing/the-groaning-worm-part-ii",
+      extract:
+        "The magicians and phonetic monsters / Have their ministries; they waste alongside the terror, / Never tasting it; their exhortations are faltering",
     },
     {
       title: "and so the wheel turns",
       slug: "writing/and-so-the-wheel-turns",
+      extract:
+        "And so the wheel turns. / The bracken breaks beneath my feet, / As these words roll over my tongue",
     },
     {
       title: "REC",
       slug: "writing/rec",
+      summary:
+        "A cameraman, interrupted from filming a new skate video by a streak of fire in the sky, documents his final hours.",
     },
     {
       title: "under the light of a dying star",
       slug: "writing/under-the-light-of-a-dying-star",
+      extract:
+        "Under the light of a dying star, Aaron McBarnes sits in front of his television. The show he’s watching is hilarious, sending him into fits of laughter, reminding him of his grandmother",
     },
     {
       title: "for your safety",
       slug: "writing/for-your-safety",
+      extract:
+        "Thirty seven thousand feet. / THE VOMIT-METER, / says the mauve paper in the seat in front",
     },
     {
       title: "the tree",
       slug: "writing/the-tree",
+      extract:
+        "The tree beckons. / Its twisted limbs reaching out, / calling, / summoning",
     },
     {
       title: "face",
       slug: "writing/face",
+      extract: "I wear it. / The face of happiness, / while it boils beneath",
     },
     {
       title: "sessions",
       slug: "writing/sessions",
+      extract:
+        "Thoughts and feelings, / Overwhelming. passions and / Truths. multiple and deafening",
     },
   ].sort((a, b) => (a.title.toLowerCase() > b.title.toLowerCase() ? 1 : -1));
 </script>
@@ -47,6 +67,12 @@
         <h2>
           {page.title}
         </h2>
+
+        {#if page.extract}
+          <p>{page.extract}…</p>
+        {:else if page.summary}
+          <p>{page.summary}</p>
+        {/if}
       </a>
     </li>
   {/each}
@@ -62,6 +88,12 @@
     font-size: 1.4em;
     margin-top: 0;
     margin-bottom: 0.2em;
+  }
+
+  li p {
+    max-width: 30em;
+    margin: 0;
+    color: rgba(var(--text-colour), 0.8);
   }
 
   li a {
